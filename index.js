e7ee43c7e812962e4de6f94ce576d15f30e4644e
opaismoe <wiebe.buursink@gmail.com>
2018-02-13 15:36:28 +0100

    Add counter to topics

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ mup.stream("/2/rsvps", stream => {
     }).on("error", e => {
        console.log("error! " + e)
     });
-});   
+});
+
+let topicsCounter = {}
+
+mup.stream("/2/rsvps", stream => {
+  stream
+    .on("data", item => {
+      console.log("got item " + item)
+
+      // inside of our stream event handler (!) we retrieve the group topics
+      const topicNames = item.group.group_topics.map(topic => topic.topic_name)
+      topicNames.forEach(name => {
+        if (topicsCounter[name]) {
+          topicsCounter[name]++
+        }
+        else {
+          topicsCounter[name] = 1
+        }
+      })
+      console.log(topicsCounter)
+
+    }).on("error", e => {
+       console.log("error! " + e)
+    });
+});
